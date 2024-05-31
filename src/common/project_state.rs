0861b7793570cb0b8bd032858db576e50427ceae
Simen Storesund <simen.st@hotmail.com>
2024-05-31 10:43:37 +0200

    Add load project func

--- src/common/project_state.rs
+++ src/common/project_state.rs
@@ -1,52 +1,99 @@
-use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, ffi::OsStr, path::Path};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Default)]
 pub struct ProjectState {
     pub maps: HashMap<MapId, Map>,
     pub current_map: MapId,
-    pub previous_map: MapId,
+    pub map_history_stack: Vec<MapId>,
 }
 
 impl ProjectState {
-    pub fn current_map(&self) -> &Map
-    {
+    pub fn current_map(&self) -> &Map {
         return self.maps.get(&self.current_map).unwrap();
     }
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct Map{   
-    pub markers: Vec<Marker>,
-    pub map_info: MapInfo,
-    pub image: String,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize, Hash, PartialEq, Eq)]
-pub struct MapId(pub u64);
-
-impl MapId{
-    pub fn new(raw: u64)->Self{
-        Self{
-            0: raw,
+    pub fn load(project_dir: &Path) -> Self {
+        let maps: HashMap<_, _> = project_dir
+            .into_iter()
+            .filter_map(|file_path| {
+                let FileType::Map(id) = get_filetype(file_path) else {
+                    return None;
+                };
+                let map = Map::load(file_path)?;
+                Some((id, map))
+            })
+            .collect();
+        Self {
+            maps,
+            ..Default::default()
         }
     }
 }
 
+pub enum FileType {
+    Map(MapId),
+    Marker(MarkerId),
+    None,
+}
+
+pub fn get_filetype(file_path: &OsStr) -> FileType {
+    let cow = file_path.to_string_lossy();
+    let mut file_name_iter = cow.split("-");
+    match (
+        file_name_iter.next(),
+        file_name_iter
+            .next()
+            .and_then(|id_string| id_string.parse::<u64>().ok()),
+    ) {
+        (Some("map"), Some(id)) => FileType::Map(MapId::new(id)),
+        (Some("marker"), Some(id)) => FileType::Marker(MarkerId::new(id)),
+        _ => FileType::None,
+    }
+}
+
 #[derive(Serialize, Deserialize)]
-pub struct Marker{
+pub struct Map {
+    pub markers: HashMap<MarkerId, Marker>,
+    pub map_info: MapInfo,
+    pub image: String,
+}
+
+impl Map {
+    pub fn load(filepath: &OsStr) -> Option<Map> {
+        None
+    }
+}
+
+macro_rules! define_id {
+    ($name:ident) => {
+        #[derive(Debug, Clone, Serialize, Deserialize, Hash, PartialEq, Eq, Default)]
+        pub struct $name(pub u64);
+
+        impl $name {
+            pub fn new(raw: u64) -> Self {
+                Self(raw)
+            }
+        }
+    };
+}
+
+define_id!(MapId);
+define_id!(MarkerId);
+
+#[derive(Serialize, Deserialize)]
+pub struct Marker {
     pub map_id: MapId,
     pub position: Position,
     pub image: String,
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct Position{
+pub struct Position {
     pub x: f32,
     pub y: f32,
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct MapInfo{
+pub struct MapInfo {
     pub content: String,
 }
--- src/ggez_gui/main.rs
+++ src/ggez_gui/main.rs
@@ -1,9 +1,17 @@
-use std::{collections::{hash_map::Entry, HashMap}, marker};
+use std::{
+    collections::{hash_map::Entry, HashMap},
+    marker,
+};
 mod config;
+use common::project_state::{self, Map, MapId, MapInfo, Marker, MarkerId, Position, ProjectState};
 use config::MARKER_SIZE;
-use common::project_state::{self, Map, MapId, MapInfo, Marker, Position, ProjectState};
 use ggez::{
-    event::{self, Button}, glam::Vec2, graphics::{self, DrawParam}, mint::{self, Point2, Vector2}, Context, GameError, GameResult
+    context::Has,
+    event::{self, Button},
+    glam::Vec2,
+    graphics::{self, DrawParam},
+    mint::{self, Point2, Vector2},
+    Context, GameError, GameResult,
 };
 use serde_json::{from_slice, map::OccupiedEntry};
 use slint::Image;
@@ -14,67 +22,90 @@ struct MainState {
 }
 
 impl MainState {
-    fn new(ctx: &mut Context) -> GameResult<Self> {
-
+    fn new(_ctx: &mut Context) -> GameResult<Self> {
         let mut maps = HashMap::<MapId, Map>::new();
-        let map0 = Map {
-            markers: vec![Marker{
+        let mut map0 = Map {
+            markers: HashMap::new(),
+            map_info: MapInfo {
+                content: "".to_string(),
+            },
+            image: "/map.jpg".to_string(),
+        };
+        map0.markers.insert(
+            MarkerId::new(0),
+            Marker {
                 map_id: MapId::new(1),
-                position: Position{x: 0.0, y: 0.0},
+                position: Position { x: 875.2, y: 860.0 },
                 image: "/marker.png".to_string(),
-            }],
+            },
+        );
+
+        let mut map1 = Map {
+            markers: HashMap::new(),
             map_info: MapInfo {
                 content: "".to_string(),
             },
             image: "/map.jpg".to_string(),
         };
-        let map1 = Map {
-            markers: vec![Marker{
+        map1.markers.insert(
+            MarkerId::new(1),
+            Marker {
                 map_id: MapId::new(0),
-                position: Position{x: 100.0, y: 100.0},
+                position: Position {
+                    x: 1290.0,
+                    y: 530.0,
+                },
                 image: "/marker.png".to_string(),
-            }],
-            map_info: MapInfo {
-                content: "".to_string(),
             },
-            image: "/map.jpg".to_string(),
-        };
-        maps.insert(MapId::new( 0 ), map0);
-        maps.insert(MapId::new( 1 ), map1);
+        );
+
+        maps.insert(MapId::new(0), map0);
+        maps.insert(MapId::new(1), map1);
 
         Ok(MainState {
             images: HashMap::new(),
-            project_state: ProjectState { maps, current_map: MapId(0), previous_map: MapId(0) },
+            project_state: ProjectState {
+                maps,
+                current_map: MapId(0),
+                map_history_stack: Vec::new(),
+            },
         })
     }
-    fn get_image(&mut self, ctx: &Context, path: &String) -> &graphics::Image
-    {
-        self.images.entry(path.clone()).or_insert_with(||graphics::Image::from_path(ctx, path).unwrap())
+    fn get_image(&mut self, ctx: &Context, path: &String) -> &graphics::Image {
+        self.images
+            .entry(path.clone())
+            .or_insert_with(|| graphics::Image::from_path(ctx, path).unwrap())
     }
 }
 
-
 impl event::EventHandler<ggez::GameError> for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-
         let position: Vec2 = ctx.mouse.position().into();
-        let map = self.project_state.maps.get(&self.project_state.current_map).unwrap();
-        
-        if  ctx.mouse.button_just_released(event::MouseButton::Left){
+        let map = self
+            .project_state
+            .maps
+            .get(&self.project_state.current_map)
+            .unwrap();
+
+        if ctx.mouse.button_just_released(event::MouseButton::Left) {
             println!("len");
-            if let Some(hovered_marker) = map.markers.iter().find(|marker|{
+            if let Some((_, hovered_marker)) = map.markers.iter().find(|(_, marker)| {
                 (Vec2::new(marker.position.x, marker.position.y) - position).length() < MARKER_SIZE
-            }){
+            }) {
                 println!("Clicked, new_map_id {:?}", hovered_marker.map_id.clone());
-                self.project_state.previous_map = self.project_state.current_map.clone();
+                self.project_state
+                    .map_history_stack
+                    .push(self.project_state.current_map.clone());
                 self.project_state.current_map = hovered_marker.map_id.clone();
-            }       
+            }
         }
-        if ctx.keyboard.is_key_just_pressed(ggez::input::keyboard::KeyCode::Back)
+        if ctx
+            .keyboard
+            .is_key_just_pressed(ggez::input::keyboard::KeyCode::Back)
         {
-            let temp = self.project_state.current_map.clone();
-            self.project_state.current_map = self.project_state.previous_map.clone();
-            self.project_state.previous_map = temp;
+            if let Some(previous) = self.project_state.map_history_stack.pop() {
+                self.project_state.current_map = previous;
+            }
         }
 
         Ok(())
@@ -87,11 +118,22 @@ impl event::EventHandler<ggez::GameError> for MainState {
         let image = self.get_image(ctx, &self.project_state.current_map().image.clone());
         canvas.draw(image, graphics::DrawParam::default());
 
-        for marker in &self.project_state.maps.get(&self.project_state.current_map).unwrap().markers{
-            let position = mint::Point2::<f32>{x : marker.position.x, y: marker.position.y};
+        for (_, marker) in &self
+            .project_state
+            .maps
+            .get(&self.project_state.current_map)
+            .unwrap()
+            .markers
+        {
+            let position = mint::Point2::<f32> {
+                x: marker.position.x,
+                y: marker.position.y,
+            };
             canvas.draw(
-            &graphics::Image::from_path(ctx, &marker.image)?, 
-                DrawParam::default().dest(position).offset(Point2{x: 0.5, y: 0.5})
+                &graphics::Image::from_path(ctx, &marker.image)?,
+                DrawParam::default()
+                    .dest(position)
+                    .offset(Point2 { x: 0.5, y: 1.0 }),
             );
         }
 
